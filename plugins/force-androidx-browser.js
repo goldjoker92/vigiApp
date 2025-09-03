b0c6960a216821b3612ed0022c216ee5c19de3af
Goldjoker92 <46529187+goldjoker92@users.noreply.github.com>
2025-09-03 00:55:33 -0300

    feat: Add force-androidx-browser plugin to inject AndroidX browser dependency

---
+++ plugins/force-androidx-browser.js
@@ -0,0 +1,16 @@
+﻿// plugins/force-androidx-browser.js
+const { withAppBuildGradle } = require('@expo/config-plugins');
+
+// Injecte : implementation('androidx.browser:browser:1.9.1') { force = true }
+module.exports = (config) =>
+  withAppBuildGradle(config, (cfg) => {
+    const addLine =
+      "implementation('androidx.browser:browser:1.9.1') { force = true }";
+    if (!cfg.modResults.contents.includes(addLine)) {
+      cfg.modResults.contents = cfg.modResults.contents.replace(
+        /dependencies\s*\{/,
+        `dependencies {\n    ${addLine}`
+      );
+    }
+    return cfg;
+  });
