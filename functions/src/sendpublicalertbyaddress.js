ded427ee17caf5f2ba8f6f44a9a10803b6d2f0aa
Goldjoker92 <46529187+goldjoker92@users.noreply.github.com>
2025-10-31 08:23:25 -0300

    fix: correct longitude calculation in haversine function

--- functions/src/sendpublicalertbyaddress.js
+++ functions/src/sendpublicalertbyaddress.js
@@ -96,7 +96,7 @@ function haversineMeters(lat1, lon1, lat2, lon2) {
   const R = 6371000,
     toRad = (d) => (d * Math.PI) / 180;
   const dLat = toRad(lat2 - lat1),
-    dLon = toRad(lat2 - lon1);
+    dLon = toRad(lon2 - lon1);
   const a =
     Math.sin(dLat / 2) ** 2 +
     Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon / 2) ** 2;
